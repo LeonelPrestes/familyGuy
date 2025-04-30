e79a5c58842e3671b7d619452b49beea05f11f75
LeonelPrestes <leonelprestes90@gmail.com>
2025-04-30 20:26:38 -0300

    Criado o componente getAllCharacters dentro da lib. Close#10

--- app/page.jsx
+++ app/page.jsx
@@ -8,15 +8,7 @@ Renders a Next.js page component that displays a grid of character avatars with 
 import { Container } from '@/components'
 import Image from 'next/image'
 import Link from 'next/link'
-import { endpoint } from '@/utils/endpoint'
-
-async function getAllCharacters() {
-  const data = await fetch(`${endpoint}/characters`)
-  if (!data.ok) {
-    throw new Error('Failed to fetch characters')
-  }
-  return data.json()
-}
+import { getAllCharacters } from '@/lib/characters'
 
 export default async function Page() {
   const data = await getAllCharacters()
