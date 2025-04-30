c32510ed4c1212a4f2c441a09baa2882c4192da3
LeonelPrestes <leonelprestes90@gmail.com>
2025-04-29 23:55:55 -0300

    Feito instalação das dependências e iniciado o app. Closes#1

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -10,5 +10,9 @@ import Image from 'next/image'
 import { TbArrowBigRightFilled } from 'react-icons/tb'
 
 export const Navigation = () => {
-  return <nav>navigation component</nav>
+  return (
+    <div className="sticky top-0 backdrop-blur-xl bg-[rgba(0,0,0,0.8)] border-b border-slate-800 z-50">
+
+    </div>
+  )
 }
